1f1fdffb4bd60caf74fa2799099cfc856dd2e285
harnish665 <84714021+harnish665@users.noreply.github.com>
2022-10-05 21:40:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector("button").addEventListener("click", clickHandler);
+function clickHandler()	{
+  document.querySelector("button").innerHTML = "A warm Welcome to my page 💙";
+  document.querySelector("button").style.textColor = "white";
+}
